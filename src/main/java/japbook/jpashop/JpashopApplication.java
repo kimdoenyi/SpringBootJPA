31c71c3e7bdbb3eb42a753cd68765f93f514580b
김진성 <kjs4082@naver.com>
2022-01-21 15:47:44 +0900

    Initial commit

---
+++ src/main/java/japbook/jpashop/JpashopApplication.java
@@ -0,0 +1,17 @@
+package japbook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+
+		System.out.println("hello = " + hello.getData());
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
