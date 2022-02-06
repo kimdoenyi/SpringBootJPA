239f7221b18648e24b17f16f0aa34f8b135b5242
김진성 <kjs4082@naver.com>
2022-02-06 14:55:48 +0900

    엔티티 클래스 개발 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,16 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    /**
+     * public 으로 선언하면 다른곳에서 호출될 수 있으니까 JPA 에서 protected 까지만 허용
+     */
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
