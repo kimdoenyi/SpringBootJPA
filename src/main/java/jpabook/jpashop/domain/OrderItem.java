b20115e25f6772e008ec9e940f3a4c5fabfd9bb4
김진성 <kjs4082@naver.com>
2022-02-21 15:57:26 +0900

    update Order and OrderItem Entity

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,8 +25,18 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count;  // 주문 수량
 
-    // == 비즈니스 로직 ==
+    // == 생성 메서드 ==
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
 
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // == 비즈니스 로직 ==
     /**
      * 재고 수량을 원복해준다
      */
