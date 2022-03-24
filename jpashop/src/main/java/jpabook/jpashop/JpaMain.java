5e8901f9067a1007d410ed94048eba11d7040cda
김진성 <kjs4082@naver.com>
2022-03-25 00:02:15 +0900

    new files

---
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();   //  고객의 요청이 올때마다 생겼다가 버렸다가 생겼다가 버렸다
+
+        EntityTransaction tx = em.getTransaction(); // db connection 을 얻는다고 생각하면 됨
+        tx.begin();
+
+        // code
+        try {
+
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        // code end
+        emf.close();
+    }
+}
