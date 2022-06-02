c58e8d3a92d0f5d2049f47ef0ffc6135ac07b9f9
김진성 <kjs4082@naver.com>
2022-06-02 21:16:53 +0900

    수정

--- src/main/java/jpabook/jpashop/InitDb.java
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -9,9 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 
-/**
- *
- */
 @Component
 @RequiredArgsConstructor
 public class InitDb {
